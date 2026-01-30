138accecf4c11076f101e32d110c63917ab6206c
lfcandido <luizfkz3@gmail.com>
2026-01-30 16:48:18 -0300

    feat - criacao de tela "Em Desenvolvimento"

---
+++ resources/js/pages/EmDesenvolvimento.tsx
@@ -0,0 +1,22 @@
+import { Head } from '@inertiajs/react';
+import AppLayout from '@/layouts/app-layout';
+
+
+export default function EmDesenvolvimento() {
+    return (
+        <AppLayout title="Relatorios" date={new Date()} >
+            <Head title="Relatorios" />
+
+            <div className="flex h-screen items-center justify-center overflow-hidden">
+                <div className="text-center">
+                    <h1 className="text-3xl text-black font-bold mb-2">🚧 Em desenvolvimento</h1>
+                    <p className="text-black">
+                        Esta funcionalidade ainda está sendo construída.
+                    </p>
+                </div>
+            </div>
+
+        </AppLayout>
+
+    )
+}
